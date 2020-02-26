f870cedc49df05bcffcc05e270972276a3fe846c
Hieu Pham <phthhieu@gmail.com>
2020-02-26 07:52:02 +0700

    Add script to quickly add a new binding

---
+++ script/create-binding.js
@@ -0,0 +1,145 @@
+const fs = require("fs");
+
+const CUSTOM_TYPE = ["color", "size", "variantColor"];
+
+const inquirer = require("inquirer");
+inquirer.registerPrompt("recursive", require("inquirer-recursive"));
+
+const createExternalMake = (componentName, props) => {
+  return `
+[@bs.module "@chakra-ui/core"] [@react.component]
+external make:
+  (
+  ${props
+    .map(prop => {
+      const type = CUSTOM_TYPE.includes(prop.type) ? "string" : prop.type;
+      return prop.optional
+        ? `~${prop.name}: ${type}=?`
+        : `~${prop.name}: ${type}`;
+    })
+    .join(",\n")}
+  ) =>
+  React.element =
+  "${componentName}";
+  `;
+};
+
+const createMakeProps = props => {
+  return `
+let makeProps =
+    (
+  ${props
+    .map(prop => {
+      return prop.optional ? `~${prop.name}=?` : `~${prop.name}`;
+    })
+    .join(",\n")}
+    ) =>
+  makeProps(
+
+  ${props
+    .map(prop => {
+      if (CUSTOM_TYPE.includes(prop.name)) {
+        if (prop.optional) {
+          return `~${prop.name}=?Option.map(${prop.name}, ${prop.name}ToJs)`;
+        }
+
+        return `~${prop.name}=${prop.name}ToJs(${prop.name})`;
+      }
+
+      if (prop.optional) {
+        return `~${prop.name}?`;
+      }
+
+      return `~${prop.name}`;
+    })
+    .join(",\n")}
+  );
+  `;
+};
+
+const importBeltAndCustomTypes = () => {
+  return `
+open Belt;
+open Chakra__Types;
+`;
+};
+
+const createNewBinding = (componentName, props) => {
+  const hasCustomProp =
+    props.find(({ type }) => CUSTOM_TYPE.includes(type)) != null;
+  fs.writeFile(
+    `./src/Chakra__${componentName}.re`,
+    `
+${hasCustomProp ? importBeltAndCustomTypes() : ""}
+
+${createExternalMake(componentName, props)}
+
+${hasCustomProp ? createMakeProps(props) : ""}
+        `,
+    function(err, file) {
+      if (err) throw err;
+      console.log("Created new module!");
+    }
+  );
+
+  fs.appendFile(
+    "./src/Chakra.re",
+    `module ${componentName} = Chakra__${componentName};\n`,
+    function(err) {
+      if (err) throw err;
+      console.log("DONE");
+    }
+  );
+};
+
+const promp = () =>
+  inquirer
+    .prompt({
+      type: "input",
+      name: "componentName",
+      message: "Component name?"
+    })
+    .then(({ componentName }) => {
+      inquirer
+        .prompt({
+          type: "recursive",
+          message: "Add new prop?",
+          name: "props",
+          prompts: [
+            {
+              type: "input",
+              name: "name",
+              message: "Prop name?"
+            },
+            {
+              type: "confirm",
+              name: "optional",
+              message: "Optional?"
+            },
+            {
+              type: "list",
+              name: "type",
+              message: "type",
+              choices: [
+                "string",
+                "int",
+                "bool",
+                new inquirer.Separator(),
+                "ReactEvent.Form.t => unit",
+                "unit => unit",
+                "int => unit",
+                "string => unit",
+                new inquirer.Separator(),
+                "React.element",
+                new inquirer.Separator(),
+                ...CUSTOM_TYPE
+              ]
+            }
+          ]
+        })
+        .then(({ props }) => {
+          createNewBinding(componentName, props);
+        });
+    });
+
+promp();
